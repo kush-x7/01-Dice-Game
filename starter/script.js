57fc805c606f98fd176849243990345b021c7681
kushagra <kushagratripathi7@gmail.com>
2021-05-16 18:44:37 +0530

    Initial commit

---
+++ starter/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let scores, roundScore, activePlayer, dice;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+//Math.random will give 
+dice = Math.floor(Math.random() *6)
